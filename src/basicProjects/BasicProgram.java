9352a911c8328b6fbb5b82bd0396e890474234b7
Divyang Acharya <acharyadivyang789@gmail.com>
2020-09-27 18:11:21 +0530

    first commit

---
+++ src/basicProjects/BasicProgram.java
@@ -0,0 +1,15 @@
+package basicProjects;
+
+import java.util.Scanner;
+
+public class BasicProgram {
+public static void main(String arg[]){
+	System.out.print("Please enter the name");
+	Scanner sc=new Scanner(System.in);
+	String name=sc.next();
+	System.out.println("Welcome " + name);
+	
+	
+	
+}
+}
